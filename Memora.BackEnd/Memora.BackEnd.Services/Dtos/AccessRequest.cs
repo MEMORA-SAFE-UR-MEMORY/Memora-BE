9f64d9ee163a26ca4218ff343b6d3c86ebd9e282
chaulb <chouladila@gmail.com>
2025-09-25 17:01:39 +0700

    fix-param

---
+++ Memora.BackEnd/Memora.BackEnd.Services/Dtos/AccessRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Memora.BackEnd.Services.Dtos
+{
+	public class AccessRequest
+	{
+		[Required(ErrorMessage = "User name is required!")]
+		public string UserName { get; set; } = string.Empty;
+		[Required(ErrorMessage = "Password is required!")]
+		public string Password { get; set; } = string.Empty;
+	}
+}
---
+++ Memora.BackEnd/Memora.BackEnd.Services/Dtos/RefreshTokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Memora.BackEnd.Services.Dtos
+{
+	public class RefreshTokenRequest
+	{
+		[Required(ErrorMessage = "Refresh token is required!")]
+		public string RefreshToken { get; set; } = string.Empty;
+	}
+}
