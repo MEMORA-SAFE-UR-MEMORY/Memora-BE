9db281089b01f76f459d00d65e30c1df57f26e15
chaulb <chouladila@gmail.com>
2025-10-16 14:02:18 +0700

    fix: PayOS webhook

--- Memora.BackEnd/Memora.BackEnd.Services/Dtos/PayOsWebhookData.cs
+++ Memora.BackEnd/Memora.BackEnd.Services/Dtos/PayOsWebhookData.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 
 namespace Memora.BackEnd.Services.Dtos
 {
@@ -13,9 +14,43 @@ namespace Memora.BackEnd.Services.Dtos
 		[JsonPropertyName("description")]
 		public string Description { get; set; } = string.Empty;
 
-		[JsonPropertyName("status")]
-		public string Status { get; set; } = string.Empty;
+		[JsonPropertyName("accountNumber")]
+		public string AccountNumber { get; set; } = string.Empty;
 
-		// Thêm các trường khác nếu cần
+		[JsonPropertyName("reference")]
+		public string Reference { get; set; } = string.Empty;
+
+		[JsonPropertyName("transactionDateTime")]
+		public DateTime TransactionDateTime { get; set; }
+
+		[JsonPropertyName("paymentLinkId")]
+		public string PaymentLinkId { get; set; } = string.Empty;
+
+		[JsonPropertyName("code")]
+		public string Code { get; set; } = string.Empty;
+
+		[JsonPropertyName("desc")]
+		public string Desc { get; set; } = string.Empty;
+
+		[JsonPropertyName("counterAccountBankId")]
+		public string CounterAccountBankId { get; set; } = string.Empty;
+
+		[JsonPropertyName("counterAccountBankName")]
+		public string CounterAccountBankName { get; set; } = string.Empty;
+
+		[JsonPropertyName("counterAccountName")]
+		public string CounterAccountName { get; set; } = string.Empty;
+
+		[JsonPropertyName("counterAccountNumber")]
+		public string CounterAccountNumber { get; set; } = string.Empty;
+
+		[JsonPropertyName("virtualAccountName")]
+		public string VirtualAccountName { get; set; } = string.Empty;
+
+		[JsonPropertyName("virtualAccountNumber")]
+		public string VirtualAccountNumber { get; set; } = string.Empty;
+
+		[JsonPropertyName("currency")]
+		public string Currency { get; set; } = string.Empty;
 	}
 }
